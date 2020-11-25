c57dc5ceca596f793fa5369c2bb0fd96cd4e2e8e
Juan Michellangelo <jbarreto2105@gmail.com>
2020-11-24 22:24:27 -0300

    Criação da pasta do site

---
+++ site/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Título</title>
+</head>
+<body>
+    <h1>Título teste</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid similique deserunt maxime quas incidunt, iusto illo sit illum sint cumque dolorum, nulla voluptas fugiat inventore? In vero laboriosam necessitatibus ab.</p>
+</body>
+</html>
